333e684250107cc2b68360e36824898cb364e3e3
Piyush <piyushat25@gmail.com>
2021-11-30 18:44:00 +0530

    1st program

---
+++ 1.py
@@ -0,0 +1,10 @@
+# Dictonary example
+myDict={
+    1: "Piyush",
+    2: "Hello",
+    3: "third",
+    4: "forth"
+}
+print("the options are: ",myDict.keys())
+a=int(input("Enter your option: "))
+print("The corresponding value is ",myDict.get(a))
