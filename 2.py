fafe475231a2ab024ae48be68fde11e298c79744
Piyush <piyushat25@gmail.com>
2021-11-30 19:06:31 +0530

    2nd pro

---
+++ 2.py
@@ -0,0 +1,12 @@
+# create empty dict
+# allow 4 input use key as names
+a=input("Enter ur fav lang: ")
+b=input("Enter ur fav lang: ")
+c=input("Enter ur fav lang: ")
+d=input("Enter ur fav lang: ")
+myD={}
+myD['A']=a
+myD['b']=b
+myD['c']=c
+myD['d']=d
+print(myD)
