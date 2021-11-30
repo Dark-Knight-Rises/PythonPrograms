cdc7b1e45b718befb9b34b50aa76050af3c5a596
Piyush <piyushat25@gmail.com>
2021-11-30 19:47:50 +0530

    Check the greatest no

---
+++ 3.py
@@ -0,0 +1,16 @@
+num1=int(input("Enter 1st "))
+num2=int(input("Enter 2st "))
+num3=int(input("Enter 3st "))
+num4=int(input("Enter 4st "))
+f1=num2
+f2=num4
+if(num1>num2):
+    f1=num1
+if(num3>num4):
+    f2=num3
+if(f1>f2):
+    print("The greatest no is ",f1)
+else:
+    print("The greatest no is ",f2)
+
+    
