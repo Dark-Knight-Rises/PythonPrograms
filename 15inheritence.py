b60976f4d56394797defe216364cf22707c0c9ca
Piyush <piyushat25@gmail.com>
2021-12-04 00:45:45 +0530

    inheritence

---
+++ 15inheritence.py
@@ -0,0 +1,11 @@
+class Employee:
+    total=0
+    def __init__(self,sal,incr):
+        self.sal=sal
+        self.incr=incr
+    def getTotal(self):
+        # Employee.total+=self.sal+self.incr
+        return(self.sal*self.incr)
+
+ob1=Employee(1000,1.5)
+print("The total sal is: ",ob1.getTotal())
